98eb7cd5400f78cdff3bb71e4abbd52645165f39
Tahsin <tahsingenuies@gmail.com>
2019-05-19 08:21:45 +0700

    basic grpc

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-const grpc = require('grpc');
-const protoLoader = require('@grpc/proto-loader');
-// const notesProto = grpc.load('notes.proto');
-const notesProto = protoLoader.loadSync('notes.proto');
-const { NoteService: notesProtoService } = notesProto;
+const grpc = require("grpc");
+const protoLoader = require("@grpc/proto-loader");
+const packageDefinition = protoLoader.loadSync("notes.proto"); // If I take service directly from here, I dont need to call .service.
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+const { NoteService } = protoDescriptor;
+
 const notes = [
-  { id: '1', title: 'Note 1', content: 'Content 1' },
-  { id: '2', title: 'Note 2', content: 'Content 2' }
+  { id: "1", title: "Note 1", content: "Content 1" },
+  { id: "2", title: "Note 2", content: "Content 2" }
 ];
 
 const server = new grpc.Server();
-server.addService(notesProtoService, {
+server.addService(NoteService.service, {
   list: (_, cb) => {
     cb(null, { notes });
   },
@@ -27,6 +28,6 @@ server.addService(notesProtoService, {
     cb(null, { success: item ? true : false });
   }
 });
-server.bind('127.0.0.1:50051', grpc.ServerCredentials.createInsecure());
-console.log('Server running at: 127.0.0.1:50051');
+server.bind("127.0.0.1:50051", grpc.ServerCredentials.createInsecure());
+console.log("Server running at: 127.0.0.1:50051");
 server.start();
